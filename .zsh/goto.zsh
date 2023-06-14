990834e24e5da1a606b0115b235d1c990bf93426
Stefan <106219607+MrFours@users.noreply.github.com>
2023-06-14 23:45:16 +0200

    Update goto.zsh

--- .zsh/goto.zsh
+++ .zsh/goto.zsh
@@ -1,8 +1,10 @@
-# Autoload the compinit function for on-demand loading of completion scripts.
-autoload -Uz compinit
+if [[ "$(uname)" == "Darwin" ]]; then
+    # Autoload the compinit function for on-demand loading of completion scripts.
+    autoload -Uz compinit
 
-# Initialize and enable the Zsh completion system using compinit.
-compinit
+    # Initialize and enable the Zsh completion system using compinit.
+    compinit
 
-# Source the goto.sh completion script provided by Homebrew.
-source $(brew --prefix)/etc/bash_completion.d/goto.sh
+    # Source the goto.sh completion script provided by Homebrew.
+    source $(brew --prefix)/etc/bash_completion.d/goto.sh
+fi
