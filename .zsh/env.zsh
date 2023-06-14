bbfc8ba3996cc2fe02bf493dd17d71444b7bdca3
Stefan <106219607+MrFours@users.noreply.github.com>
2023-06-15 00:27:10 +0200

    Update env.zsh

--- .zsh/env.zsh
+++ .zsh/env.zsh
@@ -1,10 +1,11 @@
 # Set the locale and character encoding.
 LC_CTYPE=en_US.UTF-8
 LC_ALL=en_US.UTF-8
+if [[ "$(uname)" == "Darwin" ]]; then
+    # Disable homebrew icons and hints
+    export HOMEBREW_NO_EMOJI=1
+    export HOMEBREW_NO_ENV_HINTS=1
 
-# Disable homebrew icons and hints
-export HOMEBREW_NO_EMOJI=1
-export HOMEBREW_NO_ENV_HINTS=1
-
-# Add homebrew to path
-export PATH=/opt/homebrew/bin:$PATH
+    # Add homebrew to path
+    export PATH=/opt/homebrew/bin:$PATH
+fi
