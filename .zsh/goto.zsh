ab0e7a5b5287c4251745688054b89348c2b91c2b
Stefan <106219607+MrFours@users.noreply.github.com>
2023-06-14 10:02:11 +0200

    Create goto.zsh

---
+++ .zsh/goto.zsh
@@ -0,0 +1,8 @@
+# Autoload the compinit function for on-demand loading of completion scripts.
+autoload -Uz compinit
+
+# Initialize and enable the Zsh completion system using compinit.
+compinit
+
+# Source the goto.sh completion script provided by Homebrew.
+source $(brew --prefix)/etc/bash_completion.d/goto.sh
