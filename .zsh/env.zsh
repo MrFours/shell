cfeb3d3cc8e0f879e9e506e75221320316a93c74
Stefan <106219607+MrFours@users.noreply.github.com>
2023-06-14 09:59:43 +0200

    Create env.zsh

---
+++ .zsh/env.zsh
@@ -0,0 +1,10 @@
+# Set the locale and character encoding.
+LC_CTYPE=en_US.UTF-8
+LC_ALL=en_US.UTF-8
+
+# Disable homebrew icons and hints
+export HOMEBREW_NO_EMOJI=1
+export HOMEBREW_NO_ENV_HINTS=1
+
+# Add homebrew to path
+export PATH=/opt/homebrew/bin:$PATH
